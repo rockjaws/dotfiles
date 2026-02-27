7e32df35304feaa0507e61b480a37111a38496f6
Oliver Mosgaard <dumbcaesar94@gmail.com>
2026-02-27 12:19:29 +0100

    removed majority of themes, not using

--- dot_config/nvim/lua/plugins/themery.lua
+++ dot_config/nvim/lua/plugins/themery.lua
@@ -4,21 +4,12 @@ return {
 	dependencies = {
 		"folke/tokyonight.nvim",
 		"ellisonleao/gruvbox.nvim",
-		"EdenEast/nightfox.nvim",
-		"catppuccin/nvim",
-		"rebelot/kanagawa.nvim",
-		"rose-pine/neovim",
 	},
 	config = function()
 		require("themery").setup({
 			themes = {
 				{ name = "Tokyo Night", colorscheme = "tokyonight" },
 				{ name = "Gruvbox Dark", colorscheme = "gruvbox" },
-				{ name = "Carbonfox", colorscheme = "carbonfox" },
-				{ name = "Catppuccin Macchiato", colorscheme = "catppuccin-macchiato" },
-				{ name = "Catppuccin Mocha", colorscheme = "catppuccin-mocha" },
-				{ name = "Kanagawa", colorscheme = "kanagawa" },
-				{ name = "Rose Pine Moon", colorscheme = "rose-pine-moon" },
 			},
 			livePreview = true,
 		})
